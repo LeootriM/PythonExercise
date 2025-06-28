494ee07ce7b03e65cd392bf06ed931dedb96625c
Student <sss@gmail.com>
2025-06-28 03:12:45 -0700

    ..

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
-from models import Developer , Project
+from models import Developer, Project
 
 app = FastAPI()
 
-#creating developer api with post method
-
 @app.post("/developers/")
-def create_developer(developer:Developer):
-    return {"message" : "developer created successfully" , "developer": developer}
-
-
-#creating projects api with post method
+def create_developer(developer: Developer):
+    return {"message": "Developer created successesfuly", "developer": developer}
 
 @app.post("/projects/")
-def create_project(project:Project):
-    return {"message" : "Project created successfully" , "Project": project}
+def create_project(project: Project):
+    return{"message": "Project created successfully", "Project": project}
+
+@app.get("/projects/")
+def get_project():
+    sample_project = Project(
+        title = "Sample Project",
+        description = "this is a sample Project",
+        language = ["python", "JavaScript"],
+        lead_developer = Developer(name="John Doe", experience=5)
+    )
+    return {"project": [sample_project]}
